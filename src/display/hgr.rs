bba39c2da659a37ed4b27798e174c4204a80d93b
Kevan Hollbach <khollbach@gmail.com>
2024-06-06 18:02:47 -0400

    HGR colors: impl flag bit

--- src/display/hgr.rs
+++ src/display/hgr.rs
@@ -1,5 +1,8 @@
 mod memory_mapping;
 
+use itertools::Itertools;
+use memory_mapping::Byte;
+
 use super::color::Color;
 
 pub const W: usize = 280;
@@ -7,34 +10,75 @@ pub const H: usize = 192;
 
 /// Black & white display.
 pub fn ram_to_dots_bw(mem: &[u8]) -> Vec<Vec<Color>> {
-    memory_mapping::ram_to_dots(mem)
+    memory_mapping::unscramble(mem)
         .into_iter()
-        .map(|row| {
-            row.into_iter()
-                .map(|dot| if dot { Color::White } else { Color::Black })
-                .collect()
-        })
+        .map(|row| row.into_iter().flat_map(|byte| byte.bits.map(bw)).collect())
         .collect()
+}
+
+fn bw(bit: bool) -> Color {
+    if bit {
+        Color::White
+    } else {
+        Color::Black
+    }
 }
 
 pub fn ram_to_dots_color(mem: &[u8]) -> Vec<Vec<Color>> {
-    memory_mapping::ram_to_dots(mem)
+    memory_mapping::unscramble(mem)
         .into_iter()
         .map(|row| {
             row.chunks(2)
-                .flat_map(|pair| color_mapping(pair.try_into().unwrap()))
+                .flat_map(|word| color_dots(word.try_into().unwrap()))
                 .collect()
         })
         .collect()
 }
 
-// todo: handle flag bit
-fn color_mapping(dots: [bool; 2]) -> [Color; 2] {
-    let c = match dots {
-        [false, false] => Color::Black,
-        [true, true] => Color::White,
-        [true, false] => Color::Purple,
-        [false, true] => Color::Green,
+fn color_dots(word: [Byte; 2]) -> [Color; 14] {
+    let [lo, hi] = word;
+
+    // We consider each adjacent pair of dots as a single colored blob. This
+    // isn't technically correct, but it's a good-enough simplification. (And
+    // it's similar to the way colors are described in the //e Technical
+    // Reference Manual, for example.)
+
+    let first_3_blobs = lo
+        .bits
+        .chunks_exact(2)
+        .map(|dots| (dots.try_into().unwrap(), lo.flag_bit));
+
+    let middle_blob = {
+        let bits = [lo.bits[6], hi.bits[0]];
+        let flag = match bits {
+            [true, false] => lo.flag_bit,
+            [false, true] => hi.flag_bit,
+            _ => lo.flag_bit,
+        };
+        (bits, flag)
+    };
+
+    let last_3_blobs = hi.bits[1..]
+        .chunks_exact(2)
+        .map(|dots| (dots.try_into().unwrap(), hi.flag_bit));
+
+    let all_blobs = first_3_blobs.chain([middle_blob]).chain(last_3_blobs);
+
+    all_blobs
+        .flat_map(color_mapping)
+        .collect_vec()
+        .try_into()
+        .unwrap()
+}
+
+fn color_mapping((bits, flag): ([bool; 2], bool)) -> [Color; 2] {
+    let c = match (bits, flag) {
+        ([false, false], _) => Color::Black,
+        ([true, true], _) => Color::White,
+        ([true, false], false) => Color::Purple,
+        ([false, true], false) => Color::Green,
+        ([true, false], true) => Color::MediumBlue,
+        ([false, true], true) => Color::Orange,
     };
     [c, c]
 }
--- src/display/hgr/memory_mapping.rs
+++ src/display/hgr/memory_mapping.rs
@@ -1,42 +1,54 @@
 use crate::display::gr;
 
-pub fn ram_to_dots(mem: &[u8]) -> Vec<Vec<bool>> {
+// Note: we could make this a wrapper around a u8, if it ends up mattering for
+// performance.
+#[derive(Debug, Clone, Copy)]
+pub struct Byte {
+    /// Selects the "color palette".
+    pub flag_bit: bool,
+    /// Left-to-right.
+    pub bits: [bool; 7],
+}
+
+pub fn unscramble(mem: &[u8]) -> Vec<Vec<Byte>> {
     assert_eq!(mem.len(), 2_usize.pow(16)); // 64 KiB
     let page1 = &mem[0x2000..0x4000];
 
     let sheets = split_sheets(page1);
     let sheets = sheets.map(gr::unscramble_bytes);
-    let sheets = sheets.map(sheet_to_dots);
+    let sheets = sheets.map(sheet_u8_to_byte);
     weave_sheets(sheets)
 }
 
 fn split_sheets(page: &[u8]) -> [&[u8]; 8] {
     assert_eq!(page.len(), 0x2000);
-    let mut out = [Default::default(); 8];
+    let mut out = [[].as_slice(); 8];
     for i in 0..8 {
         out[i] = &page[i * 0x400..][..0x400];
     }
     out
 }
 
-fn sheet_to_dots(sheet: Vec<Vec<u8>>) -> Vec<Vec<bool>> {
-    sheet.into_iter().map(line_of_dots).collect()
+fn sheet_u8_to_byte(sheet: Vec<Vec<u8>>) -> Vec<Vec<Byte>> {
+    sheet
+        .into_iter()
+        .map(|row| row.into_iter().map(Byte::new).collect())
+        .collect()
 }
 
-fn line_of_dots(row: Vec<u8>) -> Vec<bool> {
-    row.into_iter().flat_map(byte_to_dots).collect()
-}
-
-fn byte_to_dots(byte: u8) -> [bool; 7] {
-    let mut out = [false; 7];
-    for i in 0..7 {
-        let is_set = byte & 1 << i != 0;
-        out[i] = is_set;
+impl Byte {
+    fn new(byte: u8) -> Self {
+        let mut bits = [false; 7];
+        for i in 0..7 {
+            let is_set = byte & 1 << i != 0;
+            bits[i] = is_set;
+        }
+        let flag_bit = byte & 0x80 != 0;
+        Self { flag_bit, bits }
     }
-    out
 }
 
-fn weave_sheets(sheets: [Vec<Vec<bool>>; 8]) -> Vec<Vec<bool>> {
+fn weave_sheets(sheets: [Vec<Vec<Byte>>; 8]) -> Vec<Vec<Byte>> {
     // Each sheet has the same number of rows.
     let num_rows = sheets[0].len();
     debug_assert!(sheets.iter().all(|sheet| sheet.len() == num_rows));
